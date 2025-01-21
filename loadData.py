44845deb8ba3fa9828736d2cd6522601cd879bc0
Aziz2805 <151942970+Aziz2805@users.noreply.github.com>
2025-01-21 22:09:44 +0100

    Update loadData.py

--- loadData.py
+++ loadData.py
@@ -1,11 +1,20 @@
 import os
+import zipfile
 
 path = os.path.dirname(os.path.realpath(__file__))
 
-rar_path = os.path.join(path, "utils.rar")
+zip_path = os.path.join(path, "utils.zip")
 
-if os.path.exists(rar_path):
-    print(f"Le fichier {rar_path} existe.")
-    print(f"Taille du fichier : {os.path.getsize(rar_path)} octets")
+if os.path.exists(zip_path):
+    print(f"Le fichier {zip_path} existe.")
+    print(f"Taille du fichier : {os.path.getsize(zip_path)} octets")
+    
+    try:
+        # Ouvrir et extraire le fichier ZIP
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall(path)  # Extraire dans le même dossier
+        print("Extraction terminée avec succès.")
+    except zipfile.BadZipFile as e:
+        print(f"Erreur lors de l'extraction : {e}")
 else:
-    print(f"Erreur : Le fichier {rar_path} est introuvable.")
+    print(f"Erreur : Le fichier {zip_path} est introuvable.")
